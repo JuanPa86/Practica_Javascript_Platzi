53cee15d0bc0174368c1255596ad6751740ca45d
Juan Pablo Arango <juanpabloarangosanchez@gmail.com>
2022-06-09 00:56:49 -0500

    Adición de calculadoras para el triángulo y el círculo

--- figuras.js
+++ figuras.js
@@ -81,3 +81,35 @@ function calcularAreaCuadrado() {
     const area = areaCuadrado(value);
     alert(area);
 }
+
+function calcularPerimetroTriangulo() {
+    const input = document.getElementById ("InputTriangulo");
+    const value = input.value;
+
+    const perimetro = perimetroTriangulo(value);
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo() {
+    const input = document.getElementById ("InputTriangulo");
+    const value = input.value;
+
+    const area = areaTriangulo(value);
+    alert(area);
+}
+
+function calcularPerimetroCirculo() {
+    const input = document.getElementById ("InputCirculo");
+    const value = input.value;
+
+    const perimetro = perimetroCirculo(value);
+    alert(perimetro);
+}
+
+function calcularAreaCirculo() {
+    const input = document.getElementById ("InputCirculo");
+    const value = input.value;
+
+    const area = areaCirculo(value);
+    alert(area);
+}
