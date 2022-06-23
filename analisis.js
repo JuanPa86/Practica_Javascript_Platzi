26a897ae72637972e01bcfc65eeb2da7cabd176c
Juan Pablo Arango <juanpabloarangosanchez@gmail.com>
2022-06-23 12:10:25 -0500

    Top 10%  de los salarios

--- analisis.js
+++ analisis.js
@@ -1,31 +1,22 @@
-const salariosCol = colombia.map(
-    function (persona) {
-        return persona.salary;
-    }
-);
-
-const salariosColSorted = salariosCol.sort(
-    function (salaryA, salaryB) {
-        return salaryA - salaryB;
-    }
-);
-
+// Helpers
 function esPar(numero) {
- return (numero % 2 === 0);
-}
+    return (numero % 2 === 0);
+   }
+   
+   function calcularMediaAritmetica(lista) {
+   
+       const sumaLista = lista.reduce(
+           function (valorAcumulado = 0, nuevoElemento) {
+               return valorAcumulado + nuevoElemento;
+           }
+       );
+   
+       const promedioLista = sumaLista / lista.length;
+       return promedioLista;
+   }
 
-function calcularMediaAritmetica(lista) {
 
-    const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento) {
-            return valorAcumulado + nuevoElemento;
-        }
-    );
-
-    const promedioLista = sumaLista / lista.length;
-    return promedioLista;
-}
-
+//Calculadora de mediana
 function medianaSalarios(lista) {
     const mitad = parseInt(lista.length / 2);
 
@@ -39,9 +30,37 @@ function medianaSalarios(lista) {
         const personaMitad = lista[mitad];
         return personaMitad;       
     }
-}
+}   
+
+
+// Mediana General   
+const salariosCol = colombia.map(
+    function (persona) {
+        return persona.salary;
+    }
+);
+
+const salariosColSorted = salariosCol.sort(
+    function (salaryA, salaryB) {
+        return salaryA - salaryB;
+    }
+);
+
 
 const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+//Mediana del top 10%
+
+
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(
+    spliceStart,
+    spliceCount,
+);
+
+const medianaTop10Col = medianaSalarios(salariosTop10Col);
 
 console.log(
     medianaGeneralCol,
